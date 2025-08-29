b48dedc7545e3d78deff1b1bb3ed0775aeffc191
HazemSherif <hazemsherif24@gmail.com>
2025-08-29 20:00:02 +0300

    headless mode

--- src/test/java/testPackage/TestCase.java
+++ src/test/java/testPackage/TestCase.java
@@ -2,6 +2,7 @@ package testPackage;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
@@ -14,7 +15,11 @@ public class TestCase {
     WebDriver driver;
     @BeforeClass
     public void initializeDriver(){
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        if (System.getenv("CI") != null) { // Check if running in a CI environment
+            options.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage");
+        }
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
